8793394e829738fb9cc6fa64d712f7e3be10eb77
Arnel Bucio <rnlbucio@gmail.com>
2023-01-05 18:09:18 +0800

    Add movies row component

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Banner from "../components/Banner";
+import Row from "../components/Row";
 import tmdb from "../lib/tmdb";
 import { Movie } from "../typings";
 
@@ -67,7 +68,15 @@ export default async function Home() {
   return (
     <main className='relative pl-4 pb-24 lg:space-y-24 lg:pl-16'>
       <Banner netflixOriginals={netflixOriginals} />
-      <section></section>
+      <section className='md:space-y-12'>
+        <Row title='Trending Now' movies={trendingNow} />
+        <Row title='Top Rated' movies={topRated} />
+        <Row title='Action Thrillers' movies={actionMovies} />
+        <Row title='Comedies' movies={comedyMovies} />
+        <Row title='Scary Movies' movies={horrorMovies} />
+        <Row title='Romance Movies' movies={romanceMovies} />
+        <Row title='Documentaries' movies={documentaries} />
+      </section>
     </main>
   );
 }
